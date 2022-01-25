3e600dd856195b9ab8ff3ba16ffdc8fa5faa7e65
SlickTorpedo <55722867+SlickTorpedo@users.noreply.github.com>
2022-01-25 11:08:18 -0600

    Create main.java

---
+++ main.java
@@ -0,0 +1,23 @@
+package main.java;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.plugin.java.JavaPlugin;
+public class Main extends JavaPlugin {
+    @Override
+    public void onEnable() {
+    }
+    @Override
+    public void onDisable() {
+    }
+    @Override
+    public boolean onCommand(CommandSender sender,
+                             Command command,
+                             String label,
+                             String[] args) {
+        if (command.getName().equalsIgnoreCase("mycommand")) {
+            sender.sendMessage("You ran /mycommand!");
+            return true;
+        }
+        return false;
+    }
+}
